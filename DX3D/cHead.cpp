f0698e5575dae2e80996123d0bcbeba8d5264852
minpark96 <hmpark3355@gmail.com>
2024-01-29 11:55:12 +0900

    비율 수정

--- DX3D/cHead.cpp
+++ DX3D/cHead.cpp
@@ -15,10 +15,12 @@ void cHead::Setup()
 
 	D3DXMATRIXA16	matS, matT, mat;
 	D3DXMatrixScaling(&matS, 0.4f, 0.4f, 0.4f);
-	D3DXMatrixTranslation(&matT, 0.0f, 0.5f, 0.0f);
+	D3DXMatrixTranslation(&matT, 0.0f, 0.0f, 0.0f);
 	mat = matS * matT;
 	for (size_t i = 0; i < m_vecVertex.size(); ++i)
 	{
 		D3DXVec3TransformCoord(&m_vecVertex[i].p, &m_vecVertex[i].p, &mat);
 	}
+
+	m_vLocalPos.y = 0.5f;
 }
--- DX3D/cRightArm.cpp
+++ DX3D/cRightArm.cpp
@@ -14,8 +14,8 @@ void cRightArm::Setup()
 	cCubeNode::Setup();
 
 	D3DXMATRIXA16	matS, matT, mat;
-	D3DXMatrixScaling(&matS, 0.15f, 0.4f, 0.2f);
-	D3DXMatrixTranslation(&matT, 0.25f, 0.1f, 0.0f);
+	D3DXMatrixScaling(&matS, 0.15f, 0.6f, 0.2f);
+	D3DXMatrixTranslation(&matT, 0.25f, 0.0f, 0.0f);
 	mat = matS * matT;
 	for (size_t i = 0; i < m_vecVertex.size(); ++i)
 	{
